f0b36fd1e419953916b102017d6a7c776692f902
ByvPhil <163297886+ByvPhil@users.noreply.github.com>
2024-10-10 12:46:55 +0200

    Update bptest.ps1
    ajout de la récupération de l'ip publique

--- bptest.ps1
+++ bptest.ps1
@@ -1,3 +1,4 @@
+# vérification de l aprésence de speedtest.exe et téléchargement le cas échéant
 if (-not (test-path -path "$Env:temp\speedtest.exe")) 
 {
 write-host "non present"
@@ -7,6 +8,8 @@ $sWebRequest = @{
 }
 Invoke-WebRequest @sWebRequest
 }
+
+# exécution du speedtest
 $Speedtest = cmd /c "$Env:temp\speedtest.exe -f json --accept-gdpr --accept-license"
 $Download = $Speedtest.split(':')[11]
 $resultD = $Download.split(',')[0]
@@ -20,6 +23,7 @@ $ResultUtempB = ($resultU / 1000000)
 $Xupload = $ResultUtempB * 8
 $res2 = [math]::round($Xupload,2)
 
+#formatage du retout du speedtest
 $PourRG = "Download: $res1 Mb/s ;Upload: $res2 Mb/s"
 
 $array = $speedtest -split "isp"
@@ -32,6 +36,11 @@ $array3 = $Speedtest.split(':')[7]
 $resultLA = $array3.split(',')[0]
 $res3 = [math]::round($resultLA,2)
 
-write-output "$PourRG ;Ping: $res3 ms ;ISP:$resultISP"
+# Récupérer l'adresse IP publique
+$publicIP = Invoke-RestMethod -Uri "http://ipinfo.io/ip"
+
+# renvoi des résultats du script
+write-output "$PourRG ;Ping: $res3 ms ;ISP:$resultISP ;Public IP: $publicIP"
+
 # Ajoutez la commande exit pour fermer PowerShell
 exit
